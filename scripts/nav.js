873f288fd994a741f2ce7fc9cc7a4a04a74fe7a1
ozmos <osamumorozumi@gmail.com>
2018-07-11 06:20:05 +0800

    smooth scrolling and logo font

--- scripts/nav.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-    var navs = document.querySelectorAll('.nav-list');
-
-    navs.forEach(nav => nav.classList.toggle('toggle-reveal'));
-}
-
-document.querySelector('#menu-toggle')
-    .addEventListener('click', classToggle);
