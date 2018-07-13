66e478c18991505c01ec80aeaa0c224de9907b30
ozmos <osamumorozumi@gmail.com>
2018-07-13 15:42:55 +0800

    navbar background, anchor positions

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,23 @@
+const navs = document.querySelectorAll('.nav-list');
+
 function classToggle() {
-    var navs = document.querySelectorAll('.nav-list');
+
 
     navs.forEach(nav => nav.classList.toggle('toggle-reveal'));
 }
 
 document.querySelector('#menu-toggle')
     .addEventListener('click', classToggle);
+
+window.onscroll = function() { scrollBackground() };
+
+function scrollBackground() {
+    const navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.classList.add("blue");
+        navs.forEach(nav => nav.classList.add("blue"));
+    } else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
+        navbar.classList.remove("blue");
+        navs.forEach(nav => nav.classList.remove("blue"));
+    }
+}
