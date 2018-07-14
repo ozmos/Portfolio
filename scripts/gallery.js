e2346c9417a3e871bd17a2f361e93bc9d90df7fe
ozmos <osamumorozumi@gmail.com>
2018-07-14 10:36:08 +0800

    label script finished, comments field added, max characters

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,20 +1,15 @@
-var tiles = document.querySelectorAll('.project-tile');
-var titleElements = document.getElementsByClassName('project-title');
-var titles = [];
-var labels = document.createElement('span');
+! function addLabels() {
+    const tiles = document.getElementsByClassName('project-tile');
+    const titleElements = document.getElementsByClassName('project-title');
+    const titles = [];
 
-for (let i = 0; i < tiles.length; i++) {
-
-}
-tiles.forEach(tile => tile.insertBefore(labels, tile.firstChild));
-labels.forEach(label => label.classList.add('image-label'));
-for (let i = 0; i < titleElements.length; i++) {
-    titles.push(titleElements[i].textContent);
-    /* tiles[i] */
-    /*  for (var j = 0; j < titles.length; j++) {
-         labels[j].textContent = titles[j];
-     } */
-}
-
-
-console.log(labels);
+    for (let i = 0; i < titleElements.length; i++) {
+        titles.push(titleElements[i].textContent);
+        let labels = document.createElement('span');
+        labels.classList.add('image-label');
+        tiles[i].appendChild(labels);
+        for (let j = 0; j < titles.length; j++) {
+            tiles[j].lastChild.textContent = titles[j];
+        }
+    }
+}();
