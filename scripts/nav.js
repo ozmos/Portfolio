583cacc3f497f9464a898e0e882484034eefa405
ozmos <osamumorozumi@gmail.com>
2018-07-14 11:09:04 +0800

    clean up style sheet

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,6 @@
 const navs = document.querySelectorAll('.nav-list');
 
 function classToggle() {
-
-
     navs.forEach(nav => nav.classList.toggle('toggle-reveal'));
 }
 
@@ -13,10 +11,11 @@ window.onscroll = function() { scrollBackground() };
 
 function scrollBackground() {
     const navbar = document.getElementById("navbar");
+
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         navbar.classList.add("blue");
         navs.forEach(nav => nav.classList.add("blue"));
-    } else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
+    } else {
         navbar.classList.remove("blue");
         navs.forEach(nav => nav.classList.remove("blue"));
     }
